07f336db3e296e3cdb378f059933bd3d7cbf29b6
Celeste <crislynbackup@gmail.com>
2026-09-02 16:48:34 -0400

    Comments

--- Assets/ButtonClick.cs
+++ Assets/ButtonClick.cs
@@ -1,13 +1,14 @@
-using TMPro;
-using UnityEngine;
+using TMPro; //Pulling from TextMesh Pro library
+using UnityEngine; //Using Unity's Programming tools
 
-public class ButtonClick : MonoBehaviour
+//ButtonClick Class and Functions
+public class ButtonClick : MonoBehaviour // <-- MonoBehaviour allows Unity to attach script to a GameObject
 {
 
-    public TextMeshProUGUI textbox;
+    public TextMeshProUGUI textbox; // 'TextMeshProUGUI' = type; 'textbox' = variable (stores a reference to a TextMesh Pro UI text object)
 
-    public void ButtonClickMethod()
+    public void ButtonClickMethod() // 'void' = return type; 'ButtonClickMethod' = method name (a named set of instructions that does something)
     {
-        textbox.text = "i have changed";
+        textbox.text = "i have changed"; // Change the text displayed by textbox to "i have changed".
     }
 }
